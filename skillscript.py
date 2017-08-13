ff5fd9a2fb72b5cee1cd7d13b679f4233a76db9f
jlbrill <jlbrill@umich.edu>
2017-08-13 12:46:25 -0400

    first

---
+++ skillscript.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Read the CSV into a pandas data frame (df)
+#   With a df you can do many things
+#   most important: visualize data with Seaborn
+df = pd.read_csv('jobs.csv', sep=',')
+print(df)
+
+# Or export it in many ways, e.g. a list of tuples
+tuples = [tuple(x) for x in df.values]
+
+# or export it as a list of dicts
+dicts = df.to_dict().values()
